888b8ad3f8a4ea04388114874f55554c69e18562
Jonas Rodrigues <rodrigues_psi@outlook.com>
2022-09-07 17:16:53 -0300

    Atualização 07/09/2022
    Refatoração de RandomPasswordDiscovery 2.0.py

--- RandomPasswordDiscovery 2.0.py
+++ RandomPasswordDiscovery 2.0.py
@@ -2,12 +2,13 @@ import random
 import time
 
 senha_real = list()
-senha_nova = ['x', 'x', 'x', 'x']
 
-num_digito = int(input("Quantos dígitos a senha deve conter, entre 1 e 4? "))
-while num_digito <1 or num_digito > 4:
-    print('Você precisa inserir um valor entre 1 e 4')
-    num_digito = int(input('Quantos dígitos a senha deve conter, entre 1 e 4? '))
+num_digito = 0
+while num_digito < 1 or num_digito > 4:
+    try:
+        num_digito = int(input("Quantos dígitos a senha deve conter, entre 1 e 4? "))
+    except:
+        print('Favor inserir apenas números, entre 1 e 4!')
 print('Ótimo, vamos começar')
 time.sleep(1)
 print('Vou agora gerar uma senha que você deve descobrir qual é!')
@@ -15,22 +16,30 @@ time.sleep(1)
 #Gera senha conforme o input do usuário
 if num_digito == 1:
     senha_real.append(random.randint(0, 9))
+    senha_nova = ['X']
 elif num_digito == 2:
     senha_real.append(random.randint(0, 9))
     senha_real.append(random.randint(0, 9))
+    senha_nova = ['X', 'X']
 elif num_digito == 3:
     senha_real.append(random.randint(0, 9))
     senha_real.append(random.randint(0, 9))
     senha_real.append(random.randint(0, 9))
+    senha_nova = ['X', 'X', 'X']
 else:
     senha_real.append(random.randint(0, 9))
     senha_real.append(random.randint(0, 9))
     senha_real.append(random.randint(0, 9))
     senha_real.append(random.randint(0, 9))
+    senha_nova = ['X', 'X', 'X', 'X']
 time.sleep(1)
-#print(f'TESTE: {senha_real}')
 while senha_nova != senha_real:
-    palpite = int(input('Insira um número entre 0 e 9 para começar a adivinhar a senha: '))
+    palpite = -1
+    while palpite < 0 or palpite > 9:
+        try:
+            palpite = int(input('Insira um número entre 0 e 9 para começar a adivinhar a senha: '))
+        except:
+            print('Favor inserir apenas números entre 0 e 9')
     frequencia = senha_real.count(palpite)
     if frequencia == 0:
         print(f'A senha não contém nenhum número {palpite}')
@@ -73,9 +82,18 @@ while senha_nova != senha_real:
         senha_nova.pop(p4 + 1)
     time.sleep(1)
     if senha_real == senha_nova:
-        time.sleep(2)
+        time.sleep(1)
         print('Parabéns, você descobriu a senha!')
+
+    #Imprime a senha conforme o número de dígitos
     print('== SENHA ==')
-    print(f'= {senha_nova[0]} {senha_nova[1]} {senha_nova[2]} {senha_nova[3]} =')
+    if num_digito == 1:
+        print(f'   = {senha_nova[0]} =    ')
+    elif num_digito == 2:
+        print(f'  = {senha_nova[0]} {senha_nova[1]} =  ')
+    elif num_digito == 3:
+        print(f' = {senha_nova[0]} {senha_nova[1]} {senha_nova[2]} = ')
+    else:
+        print(f'= {senha_nova[0]} {senha_nova[1]} {senha_nova[2]} {senha_nova[3]} =')
     print('===========')
     time.sleep(1)
